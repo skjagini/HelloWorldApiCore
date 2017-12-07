b54eb93aede60bdb886237677e912ef3c31703ef
Sujith Jagini <skjagini@gmail.com>
2017-12-07 01:51:53 -0500

    Removing directory before updating with latest.

--- HelloWorldApiCore/Controllers/ValuesController.cs
+++ HelloWorldApiCore/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace HelloWorldApiCore.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2a" };
         }
 
         // GET api/values/5
--- HelloWorldApiCore/startApp.sh
+++ HelloWorldApiCore/startApp.sh
@@ -1,7 +1,12 @@
-﻿#!/bin/bash
+﻿#!/bin/sh
 echo "PWD"
 echo $PWD
 
+sudo rm -rf /Musiq
+
+sudo service nginx stop
 dotnet /Musiq/HelloWorldApiCore.dll 
+sudo service nginx start
+
 exit 0
 
